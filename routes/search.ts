ac12338456998e32ee739bcc98422a5f5b04f4e9
mael <mpineau@cerfos.net>
2024-02-09 16:47:08 +0100

    fix: SQLI Input validation

--- routes/search.ts
+++ routes/search.ts
@@ -20,6 +20,15 @@ module.exports = function searchProducts () {
   return (req: Request, res: Response, next: NextFunction) => {
     let criteria: any = req.query.q === 'undefined' ? '' : req.query.q ?? ''
     criteria = (criteria.length <= 200) ? criteria : criteria.substring(0, 200)
+    if(criteria.length > 1){
+      const onlyLettersPattern = /^[A-Za-z]+$/;
+
+      if(!criteria.match(onlyLettersPattern)){
+        return res.status(400).json({ err: "No special characters and no numbers, please!"})
+      }
+    }
+    
+
     models.sequelize.query(`SELECT * FROM Products WHERE ((name LIKE '%${criteria}%' OR description LIKE '%${criteria}%') AND deletedAt IS NULL) ORDER BY name`) // vuln-code-snippet vuln-line unionSqlInjectionChallenge dbSchemaChallenge
       .then(([products]: any) => {
         const dataString = JSON.stringify(products)
